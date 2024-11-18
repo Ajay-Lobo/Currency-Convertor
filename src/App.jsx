649c14bca10db956df36035a426db2d55b1035dc
Ajay-Lobo <nishanthajay1101@gmail.com>
2024-11-18 23:46:50 +0530

    custom hook created and currency api initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return <>
+  <h1 className="bg-primary text-black text-center text-3xl p-2">
+    Currency Converter
+  </h1>
+  </>;
+}
+
+export default App;
